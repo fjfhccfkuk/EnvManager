86a734aec41430ffc37953cc506c4c318f9cf888
hsx <fjfhccfkuk>
2021-08-24 17:12:45 +0800

    ddfa

--- js/db.js
+++ js/db.js
@@ -1,48 +1,63 @@
 const fs_path = require('path')
 const DB = require('electron-db');
 const path = require('path');
+const _db_devs_name = "DATABASE_DEVICES"
 
 var _dbHolder = null
-var dbLocation = path.join("/tmp/","")
+var dbLocation = path.join("./","")
 
 function db_list() {}
 
-function initDatabase(__dbname__ = 'sysdb', dbtable = 'sysinfo') {
-
-    // if (DB.valid(dbtable)) {
-    //     return _dbHolder
-    // } else {
-
-        DB.createTable(dbtable, dbLocation, (rev, msg) => {            
-            if (rev) {
-                console.log(msg)                
-            } else {
-                console.log('An error has occured. ' + msg)
-            }
-        })
-
-        // open database
-        // return DB
-    // }
+function _initDatabases(table) {
+    //_db_devs_name
+    DB.createTable(table, dbLocation, (rev, msg) => {            
+        if (rev) {
+            console.log(msg)                
+        } else {
+            console.log('An error has occured. ' + msg)
+        }
+    })
 }
 
-function setValueToDb(dbtable = 'sysinfo') {
-    // try to write data
-    {
-        let data = new Object();
-        data.name = "Wali"                    
-        data.age = 24
-        data.addr = "Beijing,chaoyang "
+function getDevsDBName() {
+    return _db_devs_name;
+}
 
+function initDatabase() {
+        _initDatabases(_db_devs_name)
+        // _initDatabases(...)
+}
+
+function setValueToDb(dbtable = '', obj = null) {
+    // try to write data
+    if (obj) {
         DB.insertTableContent(dbtable, dbLocation ,data, (ret, msg) => {
             console.log("Success: " + ret);
             console.log("Message: " + msg);
         })
+
+        return
     }
+
+    // {
+    //     let data = new Object();
+    //     data.name = "Wali"                    
+    //     data.age = 24
+    //     data.addr = "Beijing,chaoyang "
+
+    //     DB.insertTableContent(dbtable, dbLocation ,data, (ret, msg) => {
+    //         console.log("Success: " + ret);
+    //         console.log("Message: " + msg);
+    //     })
+    // }
 };
 
-function getValueFromDb(dbtable = 'sysinfo', key = null) {
+function getValueFromDb(dbtable = 'sysinfo', cb = null, key = null) {
     DB.getAll(dbtable, dbLocation,(ret, msg) => {
-        console.log("aaaaaaaaaaaaaa")
+        console.log("msg:" + msg)
+        if (ret) {
+            values = "aabbcc"
+            if (cb) {cb(msg)}
+        }
     })
 }
