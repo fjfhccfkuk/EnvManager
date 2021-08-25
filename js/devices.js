e2d0802839f2c8d2cb068a631468618e234bd7a4
hsx <fjfhccfkuk>
2021-08-25 13:07:27 +0800

    add system invoke for linux ,testing

--- js/devices.js
+++ js/devices.js
@@ -24,7 +24,36 @@ function Printer(_dev, sAddr, sLocation){
     }
 }
 
+function _sysInvoke() {
+    const { exec } = require('child_process');
+        exec('echo "dfdddddddd" > /tmp/bbz.txt', (err, stdout, stderr) => {
+        if (err) {
+            // node couldn't execute the command
+            return;
+        }
+        // list of printers with brief details
+        console.log(stdout);
+        // the *entire* stdout and stderr (buffered)
+        stdout=stdout.split("  ");
+        var printers=[];
+        j=0;
+        stdout = stdout.filter(item => item);
+        for (i = 0; i < stdout.length; i++)
+        {
+            if(stdout[i]==" \r\r\n" ||stdout[i]=="\r\r\n")
+            {
+            printers[j]=stdout[i+1];
+            j++; 
+            }
+        }
+        // list of only printers name
+        console.log(printers);
+        console.log(stderr);
+    });
+}
+
 function FuncTest() {
+    _sysInvoke()
     alert("Click")
 }
 
