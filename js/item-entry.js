76cce658a348723beb7429e6b400ca7f210dabf3
hsx <fjfhccfkuk>
2021-08-25 12:45:22 +0800

    修改文字大小、左侧栏布局

--- js/item-entry.js
+++ js/item-entry.js
@@ -1,5 +1,22 @@
+const { NodeEventEmitter } = require("electron")
 
 function ActivePage(page_addr) {
-    var ele = document.getElementById("embedFrame")
+    var ele = document.getElementById("areaContent")
+    // ele.innerHTML = page_addr
     ele.src = page_addr
+}
+
+
+function ActiveWebFrame(page_addr) {
+    //enable div-Frame, disable div-webSrc
+    document.getElementById("websrc").display = none
+    document.getElementById("webiframe").display = inline-block
+    document.getElementById("areaContent").src = page_addr
+}
+
+function ActiveWebSrc(page_addr) {
+    //enable div-webSrc, disable div-Frame
+    document.getElementById("websrc").display = inline-block
+    document.getElementById("webiframe").display = none
+    document.getElementById("areaContent").src = page_addr
 }
